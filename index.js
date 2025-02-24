c1bc7c69ba7e056c00c6fbae579b085c981d4d25
Gambo Usman <gamboyusuf101@gmail.com>
2025-02-24 21:00:59 +0100

    First Assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let num1 = 10;
+let num2 = 5;
+
+function sum(num1,num2){
+    let addition = num1 + num2;
+    return addition
+
+}
+console.log('10+5=', sum(10,5))
+
+function subtract(num1,num2){
+    let subtraction = num1 - num2;
+    return subtraction
+
+}
+console.log('10-5=', subtract(10,5))
+
+function multiple(num1,num2){
+    let multiplication = num1 * num2;
+    return multiplication
+
+}
+console.log('10*5=', multiple(10,5))
+
+function divide(num1,num2){
+    let division = num1 / num2;
+    return division
+
+}
+console.log('10/5=', divide(10,5))
+
+function mod(num1,num2){
+    let modulus = num1 % num2;
+    return modulus
+
+}
+console.log('10%5=', mod(10,5))
